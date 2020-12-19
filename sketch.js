0841551d7c5fbca44bea1ebf362f5c1380929139
priyobasu <72121579+priyobasu@users.noreply.github.com>
2020-12-19 19:39:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var form, player,game;
+var gameState=0;
+var playerCount=0;
+var database;
+
+
+function setup()
+{
+  database=firebase.database();
+  createCanvas(400,400);
+  
+  // call the game object and the functions getState and start
+}
+function draw(){
+
+
+}
